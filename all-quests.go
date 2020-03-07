f090d9891b00ae1038183b7b810d9a63cf02b3cc
Muhamed-Kanapiya <muhamed08@mail.ru>
2020-03-07 18:57:38 +0600

    Update all-quests.go

--- all-quests.go
+++ all-quests.go
@@ -114,6 +114,27 @@ func main() {
 }
 
 //compareprog
+
+Instructions
+
+Write a program that behaves like the Compare function from the Go package strings.
+
+This program prints a number after comparing two string lexicographically.
+Usage
+
+student@ubuntu:~/compareprog$ go build
+student@ubuntu:~/compareprog$ ./compareprog a b | cat -e
+-1$
+student@ubuntu:~/compareprog$ ./compareprog a a | cat -e
+0$
+student@ubuntu:~/compareprog$ ./compareprog b a | cat -e
+1$
+student@ubuntu:~/compareprog$ ./compareprog b a d | cat -e
+$
+student@ubuntu:~/compareprog$ ./compareprog | cat -e
+$
+student@ubuntu:~/compareprog$
+
 func main() {
 	defer z01.PrintRune('\n')
 	neg := "-1"
